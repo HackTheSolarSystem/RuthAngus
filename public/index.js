91a54a0f35a0774d340a9fd0bc41b140a09d47d6
William B <william@barkoffusa.com>
2019-02-09 16:35:42 -0500

    Fix broken links and have  mimic ghpages

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
-particlesJS.load('top', 'public/particlesjs-config.json', function () {
+var baseurl = {{ site.baseurl }}
+
+particlesJS.load('top', baseurl + '/public/particlesjs-config.json', function () {
 	console.log('callback - particles.js config loaded');
 });
