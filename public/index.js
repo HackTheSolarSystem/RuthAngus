18f9803e758003521f13c20853b74cf2c6c2a345
Alex Studer <astuder@notetoscreen.com>
2019-02-09 19:19:43 -0500

    add third planet, fail at pausing planets on mouseover

--- public/index.js
+++ public/index.js
@@ -1,5 +1,13 @@
-var baseurl = {{ site.baseurl }}
-
-particlesJS.load('top', baseurl + '/public/particlesjs-config.json', function () {
-	console.log('callback - particles.js config loaded');
-});
+// window.addEventListener("load", function() {
+// 	var mouseover = function(e) {
+// 		e.target.classList.add("paused");
+// 	};
+// 	var mouseout = function(e) {
+// 		e.target.classList.remove("paused");
+// 	};
+// 	var planets = document.querySelectorAll(".planet");
+// 	planets.forEach(function(planet) {
+// 		planet.addEventListener("mouseover", mouseover);
+// 		planet.addEventListener("mouseout", mouseout);
+// 	});
+// });
